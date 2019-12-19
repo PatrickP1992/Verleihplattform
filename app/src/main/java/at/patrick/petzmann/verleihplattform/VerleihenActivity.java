6bc15bc1eec07481b9e11e93d1523cc682144e86
pstep <p.stephan@gmx.at>
2019-12-19 12:25:12 +0100

    VerleihGegenstand hinzugefügt

--- app/src/main/java/at/patrick/petzmann/verleihplattform/VerleihenActivity.java
+++ app/src/main/java/at/patrick/petzmann/verleihplattform/VerleihenActivity.java
@@ -1,5 +1,7 @@
 package at.patrick.petzmann.verleihplattform;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +12,10 @@ public class VerleihenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verleihen);
+    }
+
+    public void VGegenstand(View view){
+        Intent intent = new Intent(this, VerleihGegenstand.class);
+        startActivity(intent);
     }
 }

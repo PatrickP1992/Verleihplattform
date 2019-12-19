a184926f4038ceb34dc8779a08da25c21020aa79
pstep <p.stephan@gmx.at>
2019-12-19 19:00:41 +0100

    autocomplete für Dienstleistung

--- app/src/main/java/at/patrick/petzmann/verleihplattform/VerleihDienstleistungActivity.java
+++ app/src/main/java/at/patrick/petzmann/verleihplattform/VerleihDienstleistungActivity.java
@@ -1,23 +1,23 @@
 package at.patrick.petzmann.verleihplattform;
 
-import android.content.Intent;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class VerleihDienstleistungActivity extends AppCompatActivity {
+    private static final String[] DIENSTL = new String[]{
+            "Haare schneiden","Rasen mähen","Garten umgraben","Fenster putzen","Grundreinigung für Gebäude"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verleih_dienstleistung);
-    }
 
-    public void angebotErstellt(View view)
-    {
-        Intent intent = new Intent(this, AngebotErstelltActivity.class);
-
-        startActivity(intent);
+        AutoCompleteTextView editText = findViewById(R.id.VerleihDienstleistungTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, DIENSTL);
+        editText.setAdapter(adapter);
     }
 }

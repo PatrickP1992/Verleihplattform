927901e13f6e6d7fd3937f5b8a67f68a658db0c0
pstep <p.stephan@gmx.at>
2020-01-15 14:05:19 +0100

    Filterregel Methode hinzugefügt

--- app/src/main/java/at/patrick/petzmann/verleihplattform/Klassen/User.java
+++ app/src/main/java/at/patrick/petzmann/verleihplattform/Klassen/User.java
@@ -11,6 +11,10 @@ public class User {
     private int points;
     private ArrayList<Item> gelieheneGegenstände;
 
+    private String[] itemName;
+    private int[] idNumber;
+    private int[] pictureCode;
+
     // --Konstruktor----------------------------------------------------
     public User(int id, String userName, String password) {
         this.id = id;
@@ -56,28 +60,55 @@ public class User {
 
     }
 
+    public String[] getItemName() {
+        return itemName;
+    }
+
+    public int[] getIdNumber() {
+        return idNumber;
+    }
+
+    public int[] getPictureCode() {
+        return pictureCode;
+    }
+
     public ArrayList<Item> getGelieheneGegenstände() {
         return gelieheneGegenstände;
     }
 
     public void addGelieheneGegenstände(Item item) {
-        for (Item i:gelieheneGegenstände) {
-            if(i.getId() == item.getId()){
+        for (Item i : gelieheneGegenstände) {
+            if (i.getId() == item.getId()) {
                 // to-do Error Message dass gegenstand schon in der liste ist.
                 return;
             }
         }
         this.gelieheneGegenstände.add(item);
+        filterForArrays();
     }
 
-    public void deleteGelieheneGegenstände(Item item){
-        for (Item i:gelieheneGegenstände) {
-            if(i.getId() == item.getId()){
+    public void deleteGelieheneGegenstände(Item item) {
+        for (Item i : gelieheneGegenstände) {
+            if (i.getId() == item.getId()) {
                 gelieheneGegenstände.remove(item);
+                filterForArrays();
                 return;
             }
         }
         //to-do error message dass element nicht in der Liste ist
+    }
+
+    private void filterForArrays() {
+
+        this.itemName = new String[gelieheneGegenstände.size()];
+        this.idNumber = new int[gelieheneGegenstände.size()];
+        this.pictureCode = new int[gelieheneGegenstände.size()];
+
+        for (int i = 0; i < gelieheneGegenstände.size(); i++) {
+            this.itemName[i] = gelieheneGegenstände.get(i).getName();
+            this.idNumber[i] = gelieheneGegenstände.get(i).getId();
+            this.pictureCode[i] = gelieheneGegenstände.get(i).getImageRessource();
+        }
     }
 
 }
